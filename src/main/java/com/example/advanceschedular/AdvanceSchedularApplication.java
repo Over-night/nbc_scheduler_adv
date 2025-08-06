fd7e14f7ccd5af4aac7db6210fafd5c265fa9b5c
OverNight <hidden98716@gmail.com>
2025-08-06 10:51:33 +0900

    first commit

---
+++ src/main/java/com/example/advanceschedular/AdvanceSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanceschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceSchedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceSchedularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/advanceschedular/AdvanceSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanceschedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceSchedularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
